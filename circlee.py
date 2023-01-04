a43674a18f1cb1c440ea14269b06f10fb9904b99
generali.collins <104697851+generali-collins@users.noreply.github.com>
2023-01-04 19:10:33 +0000

    Add files via upload

---
+++ circlee.py
@@ -0,0 +1,11 @@
+
+class Circle:
+    pi = 3.14
+    def __init__(self,radius = 1):
+        self.radius = radius
+        self.area = radius * radius * Circle.pi
+    def setRadius(self, newRadius):
+        self.radius = newRadius
+        self.area = newRadius * newRadius * self.pi
+    def getCircumference(self):
+        return self.radius * self.pi * 2
